d255016a90c456b0063e5870b0bb549a0f96dfc6
MukulSJoshi <75478327+MukulSJoshi@users.noreply.github.com>
2022-03-22 23:26:56 +0530

    Add files via upload

---
+++ create_object.js
@@ -0,0 +1,15 @@
+function CreateObject(arr) {
+    // Write your code here
+    const obj = {};
+
+ for (const firstName of obj) {
+      obj[firstName] = arr;
+      return CreateObject
+ }
+ for (const lastName of obj) {
+    obj[lastName] = arr;
+    return CreateObject
+ }
+}
+
+
